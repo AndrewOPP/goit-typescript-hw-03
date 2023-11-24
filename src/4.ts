dd84bd0e0f8459f6bfe0762e695c1c2c0f702220
AndrewOPP <a.vyblov2003@gmail.com>
2023-11-24 19:47:14 +0300

    done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,63 @@
-const key = new Key();
+class Key {
+  private signature: number;
 
-const house = new MyHouse(key);
-const person = new Person(key);
+  constructor() {
+    this.signature = Math.round(Math.random() * 10);
+  }
 
-house.openDoor(person.getKey());
+  getSignature(): number {
+    return this.signature;
+  }
+}
 
-house.comeIn(person);
+class Person {
+  constructor(private key: Key, public name: string) {
+    this.key = key;
+    this.name = name;
+  }
 
+  getKey(): Key {
+    return this.key;
+  }
+}
 
-export {};
+class House {
+  door: boolean;
+  key: Key;
+  tenants: any[];
+  constructor(key: Key) {
+    this.door = false;
+    this.key = key;
+    this.tenants = [];
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) this.tenants.push(person.name);
+    this.door = false;
+  }
+
+  openDoor(person: Person) {}
+}
+
+class MyHouse extends House {
+  openDoor(person: Person): void {
+    if (this.key === person.getKey()) {
+      this.door = true;
+      this.comeIn(person);
+      console.log(`Wellcome, ${person.name}`);
+      return;
+    }
+    console.log(`Sorry,${person.name}, wrong key `);
+    return;
+  }
+}
+const AndrewKey = new Key();
+
+const Andrew = new Person(AndrewKey, "Andrew");
+const Kirill = new Person(new Key(), "Kirill");
+const Ivan = new Person(AndrewKey, "Ivan");
+const AndrewHouse = new MyHouse(AndrewKey);
+
+AndrewHouse.openDoor(Andrew);
+AndrewHouse.openDoor(Kirill);
+AndrewHouse.openDoor(Ivan);
